71670481c95516d34cf04cbf70d0ce4cfd20be17
Rodrigo Gomez <rodrigogp4812@gmail.com>
2023-09-27 18:30:09 -0600

    '27/09/2023_RGP'

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'practicas_angular_01';
+  
+  conversion:string="";
+  valorG:string='';
+  resultado:number=0;
+  mensaje:string="";
+  
+  convGrados():void{
+    switch (this.conversion) {
+      case 'centi':
+        this.mensaje=(this.resultado=(parseInt(this.valorG)-32)*(5/9)) + "° C";
+        break;
+      
+      case 'fahre':
+        this.mensaje=(this.resultado=((parseInt(this.valorG)*(9/5))+32)) + "° F";
+        break;
+
+      default:
+        break;
+    }
+  }
+
 }
